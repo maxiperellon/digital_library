b464c41bcfac68222582cdb40953091a84cdfb2b
Maximiliano Perellón <you@example.com>
2019-07-05 11:57:19 -0300

    Se acomodaron botones del home

--- resources/views/home/home_library.blade.php
+++ resources/views/home/home_library.blade.php
@@ -3,18 +3,17 @@
     <div class="container-fluid text-center">
         <img src="{{ asset('/img/imagen_libro.png') }}" style="width: 30% ; height: 30%">
         <br><br>
-        <div class="container-fluid col-md-6 text-center">
+        <div class="container-fluid col-md-8 text-center">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-xs-6 col-sm-4">
                     <a class="btn btn-secondary btn-lg btn-block" href="{{ route('students.index') }}">ESTUDIANTES</a>
                 </div>
                 <br>
-                <div class="col-md-6">
+                <div class="col-xs-6 col-sm-4">
                     <a class="btn btn-secondary btn-lg btn-block" href="{{ route('books.index') }}">CARGAR LIBRO</a>
                 </div>
                 <br>
-                <div class="col-md-6">
-                    <br>
+                <div class="col-xs-6 col-sm-4">
                     <a class="btn btn-secondary btn-lg btn-block" href="{{ route('for_hire.index') }}">PRESTAMOS</a>
                 </div>
             </div>
